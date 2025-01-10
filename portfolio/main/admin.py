01ce599c4997ed9f9cb468f71b1fdd29ea152b94
Wranitz <niteshpaudel96@gmail.com>
2025-01-10 08:10:38 +0000

    added some classes for use in django admin

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project, Tag, ProjectImage
 
-# Register your models here.
+# creating a custom admin class for the tag model to display the name of the tag in the admin panel
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", "link"
+        )
+    inlines = []
+    search_fields = ("title", "description")
+    list_filter = ("tag",)
